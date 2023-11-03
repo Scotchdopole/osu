3cccbc6a22de12e1114f46225cde1fffca0a2905
Jirka Thon <thonjirka@gamil.com>
2023-11-03 09:23:29 +0100

    first commit

---
+++ res/js/script.js
@@ -0,0 +1,33 @@
+
+
+// function play(song, artist) {
+//     console.log(`Playing: ${song} by ${artist}`)
+// }
+
+// const playMusic = (song, artist) => {
+//     console.log(`Playing: ${song} by ${artist}`)
+// }
+
+// play("Never gona give you up", "Rick Astley");
+// playMusic("HAFO", "Frank Wild")
+
+
+
+function sum(a, b){
+    return a + b;
+}
+
+let resultA = sum(5, 4);
+let resultB = sum(7, 4);
+let result = sum(resultA, resultB);
+console.log(result);
+
+
+
+const ssum = (a, b) => a + b;
+
+
+let rresultA = sum(5, 4);
+let rresultB = sum(7, 4);
+let rresult = sum(rresultA, rresultB);
+console.log(rresult);
