19c63ae3c5fd97d6b8deffaf7facefa5beb5b5a6
Scotchdopole <thonjirka@gmail.com>
2023-11-03 20:43:02 +0100

    update

--- res/js/script.js
+++ res/js/script.js
@@ -11,23 +11,32 @@
 // play("Never gona give you up", "Rick Astley");
 // playMusic("HAFO", "Frank Wild")
 
+// function sum(a, b){
+//     return a + b;
+// }
 
-
-function sum(a, b){
-    return a + b;
-}
-
-let resultA = sum(5, 4);
-let resultB = sum(7, 4);
-let result = sum(resultA, resultB);
-console.log(result);
+// let resultA = sum(5, 4);
+// let resultB = sum(7, 4);
+// let result = sum(resultA, resultB);
+// console.log(result);
 
 
 
-const ssum = (a, b) => a + b;
+// const ssum = (a, b) => a + b;
 
 
-let rresultA = sum(5, 4);
-let rresultB = sum(7, 4);
-let rresult = sum(rresultA, rresultB);
-console.log(rresult);
+// let rresultA = sum(5, 4);
+// let rresultB = sum(7, 4);
+// let rresult = sum(rresultA, rresultB);
+// console.log(rresult);
+
+
+new kursor({
+    type: 4
+})
+
+new Kursor({
+    removeDefaultCursor: true
+  })
+  
+
